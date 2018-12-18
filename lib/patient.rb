d07b0510b0e273515444538153305d5540042816
Emmanuel Cole <emmanuel.c2011@gmail.com>
2018-12-18 23:01:18 +0000

    Automatically backed up by Learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -1,16 +1,18 @@
 class Patient
-  attr_accessor :name, :appointments
+  attr_accessor :name, :appointments, :doctors
   @@all = []
 
   def initialize(name)
     @@all << self
     @name = name
+    @doctors = []
     @appointments = []
   end
 
   def new_appointment(doctor, date)
     patientInstance = self
     appointInstance = Appointment.new(date, patientInstance, doctor)
+    @doctors << doctor
     @appointments << appointInstance
     appointInstance
   end
